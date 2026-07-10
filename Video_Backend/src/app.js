5f035e998c105bbfe50200f45db14c0ab5dface8
Keshav Raj <raj.keshav2004@gmail.com>
2026-07-11 03:19:39 +0530

    cors add

--- Video_Backend/src/app.js
+++ Video_Backend/src/app.js
@@ -15,7 +15,7 @@ import { errorHandler } from './middlewares/error.middleware.js'
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://videotube-auor.onrender.com",
     credentials: true
 }))
 
